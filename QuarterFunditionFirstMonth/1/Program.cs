ee428bb31a14eeda112eee4a28a812cf40dfd83c
Saed Fathi <saed.fathi@gmail.com>
2023-09-30 10:59:48 +0330

    Add files via upload

---
+++ QuarterFunditionFirstMonth/1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Please enter a string:");
+string str=Console.ReadLine();
+Console.WriteLine("Number of Character:"+str.Length);
+int countLetter=0,countDigit=0,countSpecialWords = 0;
+foreach (var item in str)
+{
+    if (Char.IsLetter(item))
+    {
+        countLetter++;
+    }
+
+    if (char.IsDigit(item))
+    {
+        countDigit++;
+    }
+    if (!char.IsLetter(item)&& !char.IsDigit(item))
+    {
+        countSpecialWords++;
+    }
+
+
+}
+
+Console.WriteLine("Number of Letters:" + countLetter);
+Console.WriteLine("Number of Digits:" + countDigit);
+Console.WriteLine("Number of SpecialWords:" + countSpecialWords);
