ab38d3fdcb3c74a81af3f446fa35f7bc42e28f37
Saed Fathi <saed.fathi@gmail.com>
2023-09-30 11:39:21 +0330

    Update Program.cs

--- QuarterFunditionFirstMonth/1/Program.cs
+++ QuarterFunditionFirstMonth/1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.Write("Please enter a string:");
+Console.Write("Please enter a string:");
 string str=Console.ReadLine();
-Console.WriteLine("Number of Character:"+str.Length);
+
 int countLetter=0,countDigit=0,countSpecialWords = 0;
 foreach (var item in str)
 {
@@ -20,7 +20,7 @@ foreach (var item in str)
 
 
 }
-
+Console.WriteLine("Number of Character:" + str.Length);
 Console.WriteLine("Number of Letters:" + countLetter);
 Console.WriteLine("Number of Digits:" + countDigit);
 Console.WriteLine("Number of SpecialWords:" + countSpecialWords);
