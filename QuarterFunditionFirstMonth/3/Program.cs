b6986bc7869b56cc6319a5ae3b2ce8768f70ce6c
Saed Fathi <saed.fathi@gmail.com>
2023-09-30 14:18:36 +0330

    Add files via upload

---
+++ QuarterFunditionFirstMonth/3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Please enter a string:");
+string str = Console.ReadLine();
+
+int lowercase = 0, uppercase = 0;
+for (var index = 0; index < str.Length; index++)
+{
+    var item = str[index];
+    if (item>='a' && item<='z')
+    {
+        lowercase++;
+    }
+
+    if (item >= 'A' && item <= 'Z')
+    {
+        uppercase++;
+    }
+
+  
+}
+
+Console.WriteLine("Number of Character:" + str.Length);
+Console.WriteLine("Number of LowerCase:" + lowercase);
+Console.WriteLine("Number of Uppercase:" + uppercase);
