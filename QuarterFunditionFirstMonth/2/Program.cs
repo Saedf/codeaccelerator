d25a9eef6ac1e6addaeb13af3e696672d161f268
Saed Fathi <saed.fathi@gmail.com>
2023-09-30 12:15:17 +0330

    Add files via upload

---
+++ QuarterFunditionFirstMonth/2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Security;
+
+Console.Write("Please enter a string:");
+string str = Console.ReadLine();
+string reverseStr=String.Empty;
+for (int i = str.Length-1; i >= 0; i--)
+{
+    reverseStr += str[i];
+}
+Console.WriteLine("Reverse of string[{0}] : "+reverseStr,str);
