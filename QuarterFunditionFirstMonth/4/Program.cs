b170eaf81ecd64db46fe2b1222aa0d665fc76e19
Saed Fathi <saed.fathi@gmail.com>
2023-10-01 13:02:48 +0330

    Add files via upload

---
+++ QuarterFunditionFirstMonth/4/Program.cs
@@ -0,0 +1,60 @@
+﻿string pathFile = "D:\\text.txt";
+var fileExists = File.Exists(pathFile);
+int countWords = 0;
+int countLineWith4Character = 0;
+string delim = " ,.";
+
+string[] fields = null;
+
+
+if (fileExists)
+{
+   // var text = File.ReadAllText(pathFile);
+    string[] lines = File.ReadAllLines(pathFile);
+    for (var index = 0; index < lines.Length; index++)
+    {
+        var line = lines[index];
+        
+        Console.WriteLine(line);
+       
+
+        fields = line.Split(delim.ToCharArray());
+        for (int i = 0; i < fields.Length; i++)
+        {
+            if (fields[i].Length>=4)
+            {
+                countLineWith4Character++;
+            }
+            countWords++;
+        }
+    }
+
+    Console.WriteLine("*****************************");
+    Console.WriteLine("Count Line:"+lines.Length);
+    Console.WriteLine("Count all of words in File :"+countWords);
+    Console.WriteLine("Count of words that are with more 4 Character :"+countLineWith4Character);
+
+}
+
+
+Console.ReadKey();
+//int lowercase = 0, uppercase = 0;
+//for (var index = 0; index < str.Length; index++)
+//{
+//    var item = str[index];
+//    if (item>='a' && item<='z')
+//    {
+//        lowercase++;
+//    }
+
+//    if (item >= 'A' && item <= 'Z')
+//    {
+//        uppercase++;
+//    }
+
+  
+//}
+
+//Console.WriteLine("Number of Character:" + str.Length);
+//Console.WriteLine("Number of LowerCase:" + lowercase);
+//Console.WriteLine("Number of Uppercase:" + uppercase);
