eedd985142395d560f5dcd012eff781c6b0bb7e5
Saed Fathi <saed.fathi@gmail.com>
2023-10-15 21:29:38 +0330

    Add files via upload

---
+++ QuarterFunditionFirstMonth/8/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+DateTime dtToday = DateTime.Now;
+int hour = dtToday.Hour;
+int min = dtToday.Minute;
+int seconds = dtToday.Second;
+int diffSedconds = 0;
+diffSedconds = (hour * 60 * 60) + (min * 60) + seconds;
+Console.WriteLine("************************");
+Console.WriteLine();
+Console.WriteLine("{0} seconds have passed From the beginning of today", diffSedconds);
+
+
